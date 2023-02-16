6052894369d4078d99acc99e3ce2d8ea2973b613
Abdelmaoula Deraoui <abdouder97@gmail.com>
2023-02-16 16:42:53 +0100

    setup main fragment and menu options

--- app/src/main/java/com/kadder/mviexample/MainActivity.kt
+++ app/src/main/java/com/kadder/mviexample/MainActivity.kt
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kader.mviexample.R
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        showMainFragment()
+    }
 
+    private fun showMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment(), "MAIN_FRAGMENT")
+            .commit()
     }
 
 
