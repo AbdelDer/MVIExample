a148f282d9d21c6497a02d33224d4d7798d05267
Abdelmaoula Deraoui <abdouder97@gmail.com>
2023-02-17 11:18:48 +0100

    add and implement new NetworkBoundResource

--- app/src/main/java/com/kadder/mviexample/repository/MainRepository.kt
+++ app/src/main/java/com/kadder/mviexample/repository/MainRepository.kt
@@ -1,68 +1,52 @@
 package com.kadder.mviexample.repository
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.kadder.mviexample.api.MVIRetrofitBuilder
+import com.kadder.mviexample.model.Photo
+import com.kadder.mviexample.model.User
 import com.kadder.mviexample.ui.main.state.MainViewState
-import com.kadder.mviexample.util.ApiEmptyResponse
-import com.kadder.mviexample.util.ApiErrorResponse
 import com.kadder.mviexample.util.ApiSuccessResponse
 import com.kadder.mviexample.util.DataState
+import com.kadder.mviexample.util.GenericApiResponse
 
 object MainRepository {
-    fun getPhotos(): LiveData<DataState<MainViewState>> {
-        return Transformations.switchMap(MVIRetrofitBuilder.apiService.getPhotos()) { apiResponse ->
-            object : LiveData<DataState<MainViewState>>() {
-                override fun onActive() {
-                    super.onActive()
-                    when (apiResponse) {
-                        is ApiSuccessResponse -> {
-                            value = DataState.data(
-                                message = null,
-                                data = MainViewState(
-                                    photos = apiResponse.body
-                                )
-                            )
-                        }
-                        is ApiErrorResponse -> {
-                            value =
-                                DataState.error(message = apiResponse.errorMessage)
-                        }
-                        is ApiEmptyResponse -> {
-                            value =
-                                DataState.error(message = "HTTP 204. Returned NOTHING!")
-                        }
-                    }
-                }
+    fun getBlogPosts(): LiveData<DataState<MainViewState>> {
+        return object : NetworkBoundResource<List<Photo>, MainViewState>() {
+
+            override fun handleApiSuccessResponse(response: ApiSuccessResponse<List<Photo>>) {
+                result.value = DataState.data(
+                    null,
+                    MainViewState(
+                        photos = response.body,
+                        user = null
+                    )
+                )
             }
-        }
+
+            override fun createCall(): LiveData<GenericApiResponse<List<Photo>>> {
+                return MVIRetrofitBuilder.apiService.getPhotos()
+            }
+
+        }.asLiveData()
     }
 
     fun getUser(userId: String): LiveData<DataState<MainViewState>> {
-        return Transformations.switchMap(MVIRetrofitBuilder.apiService.getUser(userId)) { apiResponse ->
-            object : LiveData<DataState<MainViewState>>() {
-                override fun onActive() {
-                    super.onActive()
-                    when (apiResponse) {
-                        is ApiSuccessResponse -> {
-                            value = DataState.data(
-                                message = null,
-                                data = MainViewState(
-                                    user = apiResponse.body
-                                )
-                            )
-                        }
-                        is ApiErrorResponse -> {
-                            value =
-                                DataState.error(message = apiResponse.errorMessage)
-                        }
-                        is ApiEmptyResponse -> {
-                            value =
-                                DataState.error(message = "HTTP 204. Returned NOTHING!")
-                        }
-                    }
-                }
+        return object : NetworkBoundResource<User, MainViewState>() {
+
+            override fun handleApiSuccessResponse(response: ApiSuccessResponse<User>) {
+                result.value = DataState.data(
+                    null,
+                    MainViewState(
+                        photos = null,
+                        user = response.body
+                    )
+                )
             }
-        }
+
+            override fun createCall(): LiveData<GenericApiResponse<User>> {
+                return MVIRetrofitBuilder.apiService.getUser(userId)
+            }
+
+        }.asLiveData()
     }
 }
